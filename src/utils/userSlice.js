b775215282ce65ac3dd59d5459ad72ecad1ff73b
Chandra-Sekhar-9490 <schandrasekhar9490@gmail.com>
2024-10-26 20:36:24 +0530

    Modified userSlice

--- src/utils/userSlice.js
+++ src/utils/userSlice.js
@@ -4,12 +4,9 @@ const userSlice = createSlice({
   name: "user",
   initialState: null,
   reducers: {
-    addUser: (state, action) => {
-      return action.payload;
-    },
-  },
-  removeUser: (state) => {
-    return null;
+    addUser: (state, action) => action.payload,
+
+    removeUser: (state) => null,
   },
 });
 
